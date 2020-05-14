d9eb330c3589d03ccc06c934b7aa4ac4bae73190
jemc300x <jemc300x@gmail.com>
2020-05-14 09:45:09 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../assets/img/logo.svg';
+
+const Header = () => (
+  <header>
+    <img src={logo} alt='Fylo'/>
+  </header>
+)
+
+export default Header;
---
+++ src/containers/Layout.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Home from '../pages/Home';
+
+const Layout = () => (
+  <Home />
+)
+
+export default Layout;
