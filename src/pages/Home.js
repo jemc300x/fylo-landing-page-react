384836c3e938292e394514eaab503e0fc9733149
jemc300x <jemc300x@gmail.com>
2020-05-14 11:25:30 -0500

    Sections One finished

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import Section from '../components/Section/Section';
 import illustration1 from '../assets/img/illustration-1.svg';
+import illustration2 from '../assets/img/illustration-2.svg';
 
 const Home = () => {
   return (
@@ -11,8 +12,14 @@ const Home = () => {
       imgUrl={illustration1}
       imgTextAlt='Illustration 1'
     >
-      <h1>Section 1</h1>  
-    </Section> 
+      <h1>All your files in one secure location, accessible anywhere.</h1>
+      <p>Fylo stores your most important files in one secure location. Access them wherever your need, shere and collaborate with friends, family, and co-workers.</p>
+      <form action="">
+        <input type="text" name="email" id="email" placeholder="Enter your email..." />
+        <button>Get Started</button>
+      </form>
+    </Section>
+     
     </>
   );
 }
